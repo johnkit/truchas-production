06021b707ef1812d8f0b7f3484eeca792300399d
John Tourtellott <john.tourtellott@kitware.com>
2020-11-03 18:07:05 -0500

    Add test-pages submodule and build docs/test-pages

---
+++ docs/test-pages/components/TestList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="(test, index) in tests" :key="index">
+      <a :href="test" @click.prevent="select(`${index}`)">{{test}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: {
+    tests: {
+      type: Array,
+      default: function() {return ['from prop']}
+    }
+  },
+  data: function() {
+    return {
+    }
+  },  // data
+  methods: {
+    select: function(i) {
+      let test = this.tests[i];
+      this.$emit('selected', test)
+    }
+  },  // methods
+}
+</script>
+
+<style scoped>
+</style>
